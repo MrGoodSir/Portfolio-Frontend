231f905407eea78084547fac37354016103c1b4e
Daniel Hill <daniel.s.hill01@gmail.com>
2021-12-22 14:01:11 -0700

    restyled contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,18 +1,40 @@
 function Contact() {
   return (
     <section className="contactPageSection">
-      <a href="https://www.facebook.com/daniel.hill.73932646" target="_blank" rel="noreferrer">
-        <img className="contactMeIcon" src="/images/facebook.png" alt="" />
-      </a>
-      <a href="https://github.com/MrGoodSir" target="_blank" rel="noreferrer">
-        <img className="contactMeIcon" src="/images/github.png" alt="" />
-      </a>
-      <a href="https://www.linkedin.com/in/daniel-hill-242152217/" target="_blank" rel="noreferrer">
-        <img className="contactMeIcon" src="/images/linkedin.png" alt="" />
-      </a>
-      <a href="mailto:daniel.s.hill01@gmail.com" target="_blank" rel="noreferrer">
-        <img className="contactMeIcon" src="/images/gmail.png" alt="" />
-      </a>
+      <section className="contactPageIcons">
+        <a
+          className="contactFlexContainer"
+          href="https://www.facebook.com/daniel.hill.73932646"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img className="contactMeIcon" src="/images/facebook.png" alt="" />
+        </a>
+        <a
+          className="contactFlexContainer"
+          href="https://github.com/MrGoodSir"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img className="contactMeIcon" src="/images/github.png" alt="" />
+        </a>
+        <a
+          className="contactFlexContainer"
+          href="https://www.linkedin.com/in/daniel-hill-242152217/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img className="contactMeIcon" src="/images/linkedin.png" alt="" />
+        </a>
+        <a
+          className="contactFlexContainer"
+          href="mailto:daniel.s.hill01@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img className="contactMeIcon" src="/images/gmail.png" alt="" />
+        </a>
+      </section>
       <h1>
         Lets keep this simple. Pick your prefered way of contact, and lets talk!
       </h1>
